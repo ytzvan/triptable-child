184afb2e5e4024b4c57829287267ffcfa03fdd2d
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 11:33:26 -0500

    modified algolia init

--- js/algoliaSearch.js
+++ js/algoliaSearch.js
@@ -11,16 +11,13 @@ jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.mi
 jQuery.loadScript("https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js");
 jQuery.loadScript("https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js");
 
-function initAlgolia() {
-  var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
+
+jQuery(document).ready(function(){
+	var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
   var index = client.initIndex('tours');
   var searchBox = jQuery('input[name=tour-search]');
   var autocompleteInstance = autocomplete(searchBox, {
     hint: false,
     debug: true
   });
-};
-
-jQuery(document).ready(function(){
-	initAlgolia();
 });
