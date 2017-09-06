dd298e8024d35e693b7cdfa88add69a3df649c12
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 13:12:55 -0500

    various changes

--- js/algoliaInit.js
+++ js/algoliaInit.js
@@ -11,7 +11,8 @@
   jQuery(document). ready(function(){
 
     var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
-    var index = client.initIndex('tours');
+    var destinations = client.initIndex('cities');
+    var regions = client.initIndex('provinces');
     var searchBox = jQuery('.tourmaster-quick-search-shortcode')[0];
 
     var rentalsDataset = {
