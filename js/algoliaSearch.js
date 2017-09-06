f486cebbbd18f2e8563e03454fea9097244cfa1f
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 11:35:16 -0500

    various changes

--- js/algoliaSearch.js
+++ js/algoliaSearch.js
@@ -7,17 +7,14 @@
     });
 };
 
-jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js");
+jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js"), function(){
+	var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
+  	var index = client.initIndex('tours');
+  	var searchBox = jQuery('input[name=tour-search]');
+  	var autocompleteInstance = autocomplete(searchBox, {
+    	hint: false,
+    	debug: true
+  	});
+};
 jQuery.loadScript("https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js");
 jQuery.loadScript("https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js");
-
-
-jQuery(document).ready(function(){
-	var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
-  var index = client.initIndex('tours');
-  var searchBox = jQuery('input[name=tour-search]');
-  var autocompleteInstance = autocomplete(searchBox, {
-    hint: false,
-    debug: true
-  });
-});
