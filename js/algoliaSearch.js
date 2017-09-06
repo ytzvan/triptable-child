f69c457f6b827c93dd3c9c21953b7209270956f9
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 11:47:51 -0500

    various changes

--- js/algoliaSearch.js
+++ js/algoliaSearch.js
@@ -7,8 +7,31 @@
     });
 };
 
+jQuery.loadScript("https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js");
+jQuery.loadScript("https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js");
 jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js"), function(){
+
 	var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
   	var index = client.initIndex('tours');
   	var searchBox = jQuery('input[name=tour-search]');
+
+  	var placesDataset = placesAutocompleteDataset({
+    countries: ['pa'],
+    algoliasearch: algoliasearch,
+    templates: {
+      header: '<div class="ad-example-header">Destinos</div>'
+    },
+    hitsPerPage: 3
+  });
+
+  // init
+  var autocompleteInstance = autocomplete(searchBox, {
+    hint: false,
+    debug: true,
+    cssClasses: {prefix: 'ad-example'}
+  	}, [
+    placesDataset
+  ]);
 };
+
+
