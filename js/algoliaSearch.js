59c0095f3215d18663732d7e30ee27a56f130488
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 11:56:19 -0500

    various changes

--- js/algoliaSearch.js
+++ js/algoliaSearch.js
@@ -15,6 +15,24 @@ jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.mi
   	var index = client.initIndex('tours');
   	var searchBox = jQuery('input[name=tour-search]')[0];
 
+  	var rentalsDataset = {
+    source: autocomplete.sources.hits(index, {hitsPerPage: 3}),
+    displayKey: 'name',
+    name: 'tours',
+    templates: {
+      header: '<div class="ad-example-header">Tours</div>',
+      suggestion: function(suggestion) {
+        console.log(suggestion);
+        return '<a href="/tour/' + suggestion.slug + '"><div class="ad-example-suggestion">' +
+          '<div class="row"><div class="col-md-5" style="background-image:url('+suggestion.fields.images[0].secure_url+');background-position: center;background-size: cover;height: 6em;"></div>' +
+          '<div class="col-md-7">' +
+             suggestion._highlightResult.name.value + '<br />' +
+            '<p style="color:#666"><i class="fa fa-money" style="color:#8a7676"></i> ' + suggestion.fields.price + '$ <i class="fa fa-clock-o col-xs-offset-2" style="color:#8a7676"></i> ' + suggestion.fields.duration + ' horas</p>' +
+          '</div></a>';
+      }
+    }
+  };
+
   	var placesDataset = placesAutocompleteDataset({
     countries: ['pa'],
     algoliasearch: algoliasearch,
@@ -30,7 +48,7 @@ jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.mi
     debug: true,
     cssClasses: {prefix: 'ad-example'}
   	}, [
-    placesDataset
+    rentalsDataset, placesDataset
   ]);
 };
 
