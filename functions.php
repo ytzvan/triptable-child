8044c2a90ffc3979b9299a4e69a8d20c13cd8825
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 12:39:39 -0500

    various changes

--- functions.php
+++ functions.php
@@ -14,9 +14,11 @@
 	{
 	    // or
 	    // Register the script like this for a theme:
-	    wp_register_script( 'algoliaSearch', get_stylesheet_directory_uri() . '/js/algoliaSearch.js', array (), true, true);
-	    wp_register_script( 'algoliaInit', get_stylesheet_directory_uri() . '/js/algoliaInit.js', array (), false, true);
-	    wp_enqueue_script( 'algoliaInit' );
+		wp_enqueue_script( 'algoliaSearchLiteLib', 'https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js', array (), false, true);
+		wp_enqueue_script( 'algoliaAutocompleteLib', 'https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js"', array (), false, true);
+		wp_enqueue_script( 'algoliaPlacesLib', 'https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js', array (), false, true);
+	    wp_enqueue_script( 'algoliaInit', get_stylesheet_directory_uri() . '/js/algoliaInit.js', array (), false, true);
 	}
+	
 	add_action( 'wp_enqueue_scripts', 'include_js_scripts' );
 ?>
--- js/algoliaInit.js
+++ js/algoliaInit.js
@@ -7,13 +7,10 @@
     });
 };
 
-jQuery.loadScript("https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js");
-jQuery.loadScript("https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js");
-jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js"), initSearch();
 
-function initSearch() {
+  jQuery(document). ready(function(){
 
-  var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
+    var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
     var index = client.initIndex('tours');
     var searchBox = jQuery('input[name=tour-search]')[0];
 
@@ -52,6 +49,9 @@ function initSearch() {
     }, [
     rentalsDataset, placesDataset
   ]);
-};s
+
+  });
+
+  
 
 
