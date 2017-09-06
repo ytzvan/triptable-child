47905dad201ae084c0c06b42667f1cf694c25a99
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 13:10:44 -0500

    various changes

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@
 		wp_enqueue_script( 'algoliaSearchLiteLib', 'https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js', array (), false, true);
 		wp_enqueue_script( 'algoliaAutocompleteLib', 'https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js"', array (), false, true);
 		wp_enqueue_script( 'algoliaPlacesLib', 'https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js', array (), false, true);
-	    wp_enqueue_script( 'algoliaInit', get_stylesheet_directory_uri() . '/js/algoliaInit.js', false, false, true);
+	    wp_enqueue_script( 'algoliaInit', get_stylesheet_directory_uri() . '/js/algoliaInit.js', false, null, true);
 	}
 
 	add_action( 'wp_enqueue_scripts', 'include_js_scripts' );
