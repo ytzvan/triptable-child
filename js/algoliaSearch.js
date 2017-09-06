7509854c903aaf048ee0e82d1bd3e07ccbc39bdb
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 11:30:02 -0500

    added algolia instantsearch script

--- js/algoliaSearch.js
+++ js/algoliaSearch.js
@@ -1,11 +1,16 @@
  jQuery.loadScript = function (url, callback) {
     jQuery.ajax({
-        url: "https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js",
+        url: url,
         dataType: 'script',
         success: callback,
         async: true
     });
 };
+
+jQuery.loadScript("https://cdn.jsdelivr.net/algoliasearch/3/algoliasearchLite.min.js");
+jQuery.loadScript("https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.js");
+jQuery.loadScript("https://cdn.jsdelivr.net/places.js/1/placesAutocompleteDataset.min.js");
+
 function initAlgolia() {
   var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
   var index = client.initIndex('tours');
