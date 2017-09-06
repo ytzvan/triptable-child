f161436c17277092d613d57f055b5c0032b33e8f
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 08:39:28 -0500

    initial commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+	// start your child theme code here
+	
+	// custom function to disable the trim excerpt
+	// add_filter('wp_trim_excerpt', 'custom_disable_trim_excerpt', 11, 2);
+	// function custom_disable_trim_excerpt( $excerpt, $original ){
+	// 	if( !empty($original) ){
+	// 		return $original;
+	// 	}
+	// 	return $excerpt;
+	// }	
