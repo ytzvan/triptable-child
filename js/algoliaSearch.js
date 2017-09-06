9f97b8cde788f6bb3cd99198ff67c0e7fe23b97b
Ytzvan Mastino <mastino14@gmail.com>
2017-09-06 11:20:25 -0500

    added footer option

--- js/algoliaSearch.js
+++ js/algoliaSearch.js
@@ -1,4 +1,6 @@
   (function() {
+  var client = algoliasearch('CLO2E2RKP1', 'bf4587ef5d150368765803e8c74440d0');
+  var index = client.initIndex('tours');
   var searchBox = jQuery('input[name=tour-search]');
   var autocompleteInstance = autocomplete(searchBox, {
     hint: false,
